73288004032e4301ee8f9d739cc83e9ba44c5aae
Dawid Kolankowski <nexog1997@gmail.com>
2023-01-26 22:04:54 +0100

    Add healthcheck

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import config from 'config';
 import connectDB from './utils/connectDB';
 import logger from './utils/logger';
@@ -8,6 +8,8 @@ const port = config.get<number>('port');
 const app = express();
 
 app.use(express.json());
+
+app.get('/healthcheck', (_req: Request, res: Response) => res.sendStatus(200));
 
 app.use('/api/v1/user', userRouter);
 
