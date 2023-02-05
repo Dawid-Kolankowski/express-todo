26ed25e4692d623d11f736fa4421a055d6085703
Dawid Kolankowski <nexog1997@gmail.com>
2023-02-05 19:53:48 +0100

    add task model

---
+++ src/models/task.model.ts
@@ -0,0 +1,25 @@
+import mongoose, { ObjectId, Schema } from 'mongoose';
+import config from 'config';
+
+export type ITask = {
+  title: string;
+  description: string;
+  status: boolean;
+  user: ObjectId;
+};
+
+type ITaskDocument = ITask & mongoose.Document;
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: false },
+    status: { type: Boolean, default: false, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+  },
+  { timestamps: true },
+);
+
+const TaskModel = mongoose.model<ITaskDocument>('Task', taskSchema);
+
+export default TaskModel;
