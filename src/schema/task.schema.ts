eaf8fd38710252a5d7f6db0f887c48c9667ced49
Dawid Kolankowski <nexog1997@gmail.com>
2023-02-05 19:56:43 +0100

    add task schema

---
+++ src/schema/task.schema.ts
@@ -0,0 +1,13 @@
+import { boolean, object, string, TypeOf } from 'zod';
+
+export const taskSchema = object({
+  body: object({
+    title: string({
+      required_error: 'Title is required',
+    }),
+    description: string().optional(),
+    state: boolean().optional(),
+  }),
+});
+
+export type ITaskInput = TypeOf<typeof taskSchema>['body'];
